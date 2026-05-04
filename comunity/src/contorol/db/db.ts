c7bd63b51ab736265cfc283226048041fb296ad8
yufoxda <mysubaccoun1@gmail.com>
2026-05-05 08:48:06 +0900

    update

--- comunity/src/contorol/db/db.ts
+++ comunity/src/contorol/db/db.ts
@@ -1,18 +1,1 @@
 // ここでDBクライアントを初期化して、APIハンドラーで利用できるようにした方がいい
-import type { SupabaseClient } from "@supabase/supabase-js"
-
-export function createRole(client: SupabaseClient, id: string){
-    return client.from('roles').select('*')
-}
-
-export function readRole(client: SupabaseClient, id: string){
-    return client.from('roles').select('*')
-}
-
-export function updateRole(client: SupabaseClient, id: string){
-    return client.from('roles').select('*')
-}
-
-export function deleteRole(client: SupabaseClient, id: string){
-    return client.from('roles').select('*')
-}
